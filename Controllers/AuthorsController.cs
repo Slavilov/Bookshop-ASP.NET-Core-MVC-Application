15e0c24bedfa2e650f7ba888bf628ca528ebaf6b
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-03-21 18:11:19 +0200

    Added Authors and Books Controllers and added coresponding Views for the Controllers.

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookshop_ASP.NET_Core_MVC_Application.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        // GET: AuthorsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AuthorsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AuthorsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AuthorsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookshop_ASP.NET_Core_MVC_Application.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
