1ba5361ee53a0b144fe62b92e6f06ac14e2eb0fe
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-03-21 11:30:44 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookshop_ASP.NET_Core_MVC_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
